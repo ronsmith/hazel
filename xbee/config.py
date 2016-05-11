d0fdf2675916997c452e363b51f881437b2b2ef8
Ron Smith <ron.smith@thataintworking.com>
2016-05-11 16:11:50 -0500

    added logging configuration

--- xbee/config.py
+++ xbee/config.py
@@ -3,6 +3,8 @@
 # Copyright ©2016 That Ain't Working, All Rights Reserved
 
 import os
+from logging.config import dictConfig as logger_config
+
 
 XBEE_SERIAL_PORT = os.environ.get('XBEE_SERIAL_PORT', '/dev/ttyAMA0')
 
@@ -11,3 +13,33 @@ XBEE_SERIAL_CONFIG = (XBEE_SERIAL_PORT, 9600, 8, 'N', 1)
 WEB_SERVICE_PORT = 50001
 
 BROADCAST_PORT = 50002
+
+
+logger_config({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        },
+        # 'scheduler': {
+        #     'handlers': ['default'],
+        #     'level': 'DEBUG',
+        #     'propagate': True
+        # },
+    }
+})
